d8edba0ad7a9487b35b4363301942a1bd5a781f3
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-03-18 21:59:11 +0530

    Update tsp_brute_force.c

--- tsp_brute_force.c
+++ tsp_brute_force.c
@@ -7,3 +7,19 @@
 int min(int a, int b) {
     return (a < b) ? a : b;
 }
+
+int calculateDistance(int** graph, int* path, int n) {
+    int distance = 0;
+    for (int i = 0; i < n - 1; i++) {
+        distance += graph[path[i]][path[i + 1]];
+    }
+    distance += graph[path[n - 1]][path[0]]; // Return to the starting city
+    return distance;
+}
+
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
