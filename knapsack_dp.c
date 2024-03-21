108977eda34def3642d28f69965ae0c891966ed4
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-03-21 22:23:25 +0530

    Create knapsack_dp.c

---
+++ knapsack_dp.c
@@ -0,0 +1,22 @@
+int max(int a, int b) { return (a > b) ? a : b; }
+
+int knapSack(int W, int wt[], int val[], int n)
+{
+    int i, w;
+    int K[n + 1][W + 1];
+
+    for (i = 0; i <= n; i++)
+    {
+        for (w = 0; w <= W; w++)
+        {
+            if (i == 0 || w == 0)
+                K[i][w] = 0;
+            else if (wt[i] <= w)
+                K[i][w] = max(val[i] + K[i - 1][w - wt[i]], K[i - 1][w]);
+            else
+                K[i][w] = K[i - 1][w];
+        }
+    }
+
+    return K[n][W];
+}
