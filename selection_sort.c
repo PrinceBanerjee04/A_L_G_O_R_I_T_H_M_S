1f0d8bbfc6eeb47753040fd020863dfc6ca3e6c2
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-03-22 20:23:13 +0530

    Create selection_sort.c

---
+++ selection_sort.c
@@ -0,0 +1,34 @@
+
+int main()
+{
+    int n;
+    printf("Enter Array Size : ");
+    scanf("%d",&n);
+    int arr[n],i,j,temp,min;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("Given Array : ");
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    for(i=0;i<n-1;i++)
+    {
+        min=i;
+        for(j=i+1;j<n;j++)
+        {
+            if(arr[j]<arr[min])
+            min=j;
+        }
+        temp=arr[i];
+        arr[i]=arr[min];
+        arr[min]=temp;
+    }
+    printf("\nSorted Array : ");
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+}
