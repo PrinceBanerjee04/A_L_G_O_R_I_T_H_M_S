c1e30f183a949342721eece12dae862a6017deae
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-03-23 13:17:00 +0530

    Create heap_sort.c

---
+++ heap_sort.c
@@ -0,0 +1,70 @@
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void max_heapify(int arr[], int n, int i)
+{
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[largest])
+    {
+        largest = left;
+    }
+
+    if (right < n && arr[right] > arr[largest])
+    {
+        largest = right;
+    }
+
+    if (largest != i)
+    {
+        swap(&arr[i], &arr[largest]);
+        max_heapify(arr, n, largest);
+    }
+}
+
+void build_max_heap(int arr[], int n)
+{
+    for (int i = n / 2 - 1; i >= 0; i--)
+    {
+        max_heapify(arr, n, i);
+    }
+}
+
+void heap_sort(int arr[], int n)
+{
+    build_max_heap(arr, n);
+
+    for (int i = n - 1; i > 0; i--)
+    {
+        swap(&arr[0], &arr[i]);
+        max_heapify(arr, i, 0);
+    }
+}
+
+void print_array(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int arr[] = {4, 10, 3, 5, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    heap_sort(arr, n);
+
+    printf("Sorted array: ");
+    print_array(arr, n);
+
+    return 0;
+}
