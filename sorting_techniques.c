aac364359ffb5f42132e88f5775aa318c2581325
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-03-28 19:00:11 +0530

    Update sorting_techniques.c

--- sorting_techniques.c
+++ sorting_techniques.c
@@ -18,3 +18,24 @@ void bubble_sort(int arr[], int n)
         print_array(arr, n);
     }
 }
+
+void selection_sort(int arr[], int n)
+{
+    int i, j, temp, min;
+    for (i = 0; i < n - 1; i++)
+    {
+        min = i;
+        for (j = i + 1; j < n; j++)
+        {
+            sc++;
+            if (arr[j] < arr[min])
+                min = j;
+        }
+        ss++;
+        temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+        printf("After pass %d: ", i + 1);
+        print_array(arr, n);
+    }
+}
