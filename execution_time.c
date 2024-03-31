4e4a68f2f43f30fd4f3de360fbf3c2bdf6e0dd1b
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-03-31 22:02:30 +0530

    Update execution_time.c

--- execution_time.c
+++ execution_time.c
@@ -1,3 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+void merge(int a[], int l1, int u1, int l2, int u2)
+{
+    int p, q, j, n;
+    int d[100];
+    p = l1;
+    q = l2;
+    n = 0;
+    while (p <= u1 && q <= u2)
+    {
+        if (a[p] < a[q])
+            d[n++] = a[p++];
+        else
+            d[n++] = a[q++];
+    }
+    while (p <= u1)
+    {
+        d[n++] = a[p++];
+    }
+    while (q <= u2)
+    {
+        d[n++] = a[q++];
+    }
+    for (q = l1, n = 0; q <= u2; q++, n++)
+    {
+        a[q] = d[n];
+    }
+}
