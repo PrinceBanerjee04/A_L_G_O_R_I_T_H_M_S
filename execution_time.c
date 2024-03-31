fc232a2065f78fa42929628d4907535b84a9f783
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-03-31 22:03:04 +0530

    Update execution_time.c

--- execution_time.c
+++ execution_time.c
@@ -41,3 +41,19 @@ void mergesort(int a[],int lower,int upper)
         merge(a, lower, mid, mid + 1, upper);
     }
 }
+
+void bubbleSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
