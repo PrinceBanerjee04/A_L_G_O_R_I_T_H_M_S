a39ddc71b6cca6ac4d8bfe9241439b83eac8243b
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-04-02 19:59:51 +0530

    Update tsp_greedy.c

--- tsp_greedy.c
+++ tsp_greedy.c
@@ -38,3 +38,37 @@ void nearestNeighbor(int** graph, int* path, int n) {
         currentCity = nearestCity;
     }
 }
+
+
+int main() {
+    int n;
+    printf("Enter the number of cities: ");
+    scanf("%d", &n);
+
+    int** graph = (int**)malloc(n * sizeof(int*));
+    for (int i = 0; i < n; i++) {
+        graph[i] = (int*)malloc(n * sizeof(int));
+        for (int j = 0; j < n; j++) {
+            printf("Enter the distance between city %d and city %d: ", i, j);
+            scanf("%d", &graph[i][j]);
+        }
+    }
+
+    int* path = (int*)malloc(n * sizeof(int));
+
+    nearestNeighbor(graph, path, n);
+
+    printf("Shortest Hamiltonian cycle: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", path[i]);
+    }
+    printf("\nTotal distance: %d\n", calculateDistance(graph, path, n));
+
+    for (int i = 0; i < n; i++) {
+        free(graph[i]);
+    }
+    free(graph);
+    free(path);
+
+    return 0;
+}
