d46f10ee0e6254a54a7f0e3796545ec08dce88cd
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-04-02 19:59:29 +0530

    Update tsp_greedy.c

--- tsp_greedy.c
+++ tsp_greedy.c
@@ -13,3 +13,28 @@ void swap(int* a, int* b) {
     *a = *b;
     *b = temp;
 }
+
+void nearestNeighbor(int** graph, int* path, int n) {
+    int visited[n];
+    for (int i = 0; i < n; i++) {
+        visited[i] = 0;
+    }
+
+    int currentCity = 0; // Start from city 0
+    path[0] = 0;
+    visited[0] = 1;
+
+    for (int i = 1; i < n; i++) {
+        int nearestCity = -1;
+        int minDistance = INT_MAX;
+        for (int j = 0; j < n; j++) {
+            if (!visited[j] && graph[currentCity][j] < minDistance) {
+                minDistance = graph[currentCity][j];
+                nearestCity = j;
+            }
+        }
+        path[i] = nearestCity;
+        visited[nearestCity] = 1;
+        currentCity = nearestCity;
+    }
+}
