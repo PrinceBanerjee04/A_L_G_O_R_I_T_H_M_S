d85593320cdc2a12fceb977ad6fb382a2422d391
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-04-02 20:00:07 +0530

    Update tsp_greedy.c

--- tsp_greedy.c
+++ tsp_greedy.c
@@ -1,3 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#define MAX_CITIES 10
+
+int min(int a, int b) {
+    return (a < b) ? a : b;
+}
 
 int calculateDistance(int** graph, int* path, int n) {
     int distance = 0;
