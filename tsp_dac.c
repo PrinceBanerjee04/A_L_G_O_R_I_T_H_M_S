4fff8a5da8c4d0cd31de59f72bab28b16f96d0c9
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-04-02 19:55:57 +0530

    Update tsp_dac.c

--- tsp_dac.c
+++ tsp_dac.c
@@ -1,3 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#define MAX_CITIES 10
+
+int min(int a, int b) {
+    return (a < b) ? a : b;
+}
+
 int calculateDistance(int** graph, int* path, int n) {
     int distance = 0;
     for (int i = 0; i < n - 1; i++) {
