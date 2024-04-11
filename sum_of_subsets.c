354bde67d5332dcf9ad9c820b5bbd03ded19d978
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-04-11 12:56:18 +0530

    Update sum_of_subsets.c

--- sum_of_subsets.c
+++ sum_of_subsets.c
@@ -1,1 +1,16 @@
-.
+void sum_of_subsets(int numbers[], int target_sum, int subset[], int subset_size, int index, int sum_so_far) {
+    if (sum_so_far == target_sum) {
+        printf("{ ");
+        for (int i = 0; i < subset_size; i++) {
+            printf("%d ", subset[i]);
+        }
+        printf("}\n");
+        return;
+    }
+    if (index >= sizeof(numbers)/sizeof(numbers[0]) || sum_so_far > target_sum) {
+        return;
+    }
+    sum_of_subsets(numbers, target_sum, subset, subset_size, index + 1, sum_so_far);
+    subset[subset_size] = numbers[index];
+    sum_of_subsets(numbers, target_sum, subset, subset_size + 1, index + 1, sum_so_far + numbers[index]);
+}
