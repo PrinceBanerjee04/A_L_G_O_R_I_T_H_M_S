d812103aebcd378cf33902899358323bbdcf6f69
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-04-21 02:13:08 +0530

    Update DFS.c

--- DFS.c
+++ DFS.c
@@ -1,1 +1,10 @@
+void dfs(int node) {
+    printf("%d ", node);
+    visited[node] = 1;
 
+    for (int i = 0; i < numNodes; i++) {
+        if (graph[node][i] && !visited[i]) {
+            dfs(i);
+        }
+    }
+}
