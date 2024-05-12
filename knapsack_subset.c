482449e0d889c882d217a7991d16fb4b5216b846
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-05-12 14:27:21 +0530

    Update knapsack_subset.c

--- knapsack_subset.c
+++ knapsack_subset.c
@@ -41,8 +41,7 @@ int knapsack(Item items[], int n, int capacity, int *subset)
 }
 
 int main() {
-    Item items[MAX_ITEMS] = {{2, 10},{3, 15},{5, 25},{7, 30}
-    };
+    Item items[MAX_ITEMS] = {{2, 10},{3, 15},{5, 25},{7, 30}};
 
     int n = sizeof(items) / sizeof(items[0]);
     int capacity = 10;
