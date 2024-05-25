08e22c687294caaf8cb2a68c60af085d1fe2b40f
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-05-25 20:52:12 +0530

    Update huffman_coding.c

--- huffman_coding.c
+++ huffman_coding.c
@@ -1,1 +1,18 @@
 // GREEDY METHOD
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Node structure for Huffman tree
+typedef struct Node {
+    char data;
+    unsigned freq;
+    struct Node *left, *right;
+} Node;
+
+// Structure for priority queue
+typedef struct PriorityQueue {
+    unsigned size;
+    unsigned capacity;
+    Node** array;
+} PriorityQueue;
