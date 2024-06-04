56af98400cd5c9a76f0da4376c68986dc77d5b8c
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-06-04 19:07:12 +0530

    Update kruskal's_algo.c

--- kruskal's_algo.c
+++ kruskal's_algo.c
@@ -14,3 +14,24 @@ typedef struct {
 
 int graph[MAX_NODES][MAX_NODES];
 int numNodes, numEdges;
+
+int find(Subset subsets[], int i) {
+    if (subsets[i].parent != i) {
+        subsets[i].parent = find(subsets, subsets[i].parent);
+    }
+    return subsets[i].parent;
+}
+
+void Union(Subset subsets[], int x, int y) {
+    int xroot = find(subsets, x);
+    int yroot = find(subsets, y);
+
+    if (subsets[xroot].rank < subsets[yroot].rank) {
+        subsets[xroot].parent = yroot;
+    } else if (subsets[xroot].rank > subsets[yroot].rank) {
+        subsets[yroot].parent = xroot;
+    } else {
+        subsets[yroot].parent = xroot;
+        subsets[xroot].rank++;
+    }
+}
