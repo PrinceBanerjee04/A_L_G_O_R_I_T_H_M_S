66125889a80ccbf8ff092c0525b18e1d573d78db
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-06-06 12:42:40 +0530

    Update floyd_warshall_algo.c

--- floyd_warshall_algo.c
+++ floyd_warshall_algo.c
@@ -9,7 +9,8 @@ void floydWarshall(int graph[V][V], int numVertices)
 {
     int dist[V][V];
     
-    for (int i = 0; i < numVertices; i++) {
+    for (int i = 0; i < numVertices; i++) 
+    {
         for (int j = 0; j < numVertices; j++) {
             dist[i][j] = graph[i][j];
         }
