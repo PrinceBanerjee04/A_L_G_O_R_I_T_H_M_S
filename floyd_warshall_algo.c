99092968834f2907f58698b8700d890676a06819
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-06-10 01:14:07 +0530

    Update floyd_warshall_algo.c

--- floyd_warshall_algo.c
+++ floyd_warshall_algo.c
@@ -16,3 +16,17 @@ void floydWarshall(int graph[V][V], int numVertices)
             dist[i][j] = graph[i][j];
         }
     }
+
+// Add all vertices one by one to the set of intermediate vertices.
+    for (int k = 0; k < numVertices; k++) {
+        // Pick all vertices as source one by one.
+        for (int i = 0; i < numVertices; i++) {
+            // Pick all vertices as destination for the above picked source.
+            for (int j = 0; j < numVertices; j++) {
+                // If vertex k is on the shortest path from i to j, then update the value of dist[i][j].
+                if (dist[i][k] != INT_MAX && dist[k][j] != INT_MAX && dist[i][k] + dist[k][j] < dist[i][j]) {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
