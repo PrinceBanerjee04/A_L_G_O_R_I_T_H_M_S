ec01a33e759a75a9ffde2aa4998f23f6d20dfad3
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-06-13 21:13:08 +0530

    Update floyd_warshall_algo.c

--- floyd_warshall_algo.c
+++ floyd_warshall_algo.c
@@ -60,7 +60,8 @@ int main()
     scanf("%d", &numVertices);
     int graph[V][V];
     printf("Enter the adjacency matrix (use %d for infinity):\n", INT_MAX);
-    for (int i = 0; i < numVertices; i++) {
+    for (int i = 0; i < numVertices; i++) 
+    {
         for (int j = 0; j < numVertices; j++) {
             scanf("%d", &graph[i][j]);
         }
