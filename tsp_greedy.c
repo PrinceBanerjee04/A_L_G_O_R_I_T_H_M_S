802a5d6a1aa079d9f3e0a6c7f89dac127ed43f2c
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-06-15 19:36:48 +0530

    Update tsp_greedy.c

--- tsp_greedy.c
+++ tsp_greedy.c
@@ -20,7 +20,8 @@ int calculateDistance(int** graph, int* path, int n)
     return distance;
 }
 
-void swap(int* a, int* b) {
+void swap(int* a, int* b) 
+{
     int temp = *a;
     *a = *b;
     *b = temp;
