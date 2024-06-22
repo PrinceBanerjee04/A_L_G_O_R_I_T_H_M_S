b3b70bf4efc96eada640721394829d8c2990b8d5
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-06-22 20:14:10 +0530

    Update tsp_brute_force.c

--- tsp_brute_force.c
+++ tsp_brute_force.c
@@ -57,7 +57,8 @@ int main()
     scanf("%d", &n);
 
     int** graph = (int**)malloc(n * sizeof(int*));
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) 
+    {
         graph[i] = (int*)malloc(n * sizeof(int));
         for (int j = 0; j < n; j++) {
             printf("Enter the distance between city %d and city %d: ", i, j);
