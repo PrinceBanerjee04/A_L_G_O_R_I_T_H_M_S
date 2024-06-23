63e2a172bff43e18a37858574cc91a251dcab97d
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-06-24 00:37:51 +0530

    Update BFS.c

--- BFS.c
+++ BFS.c
@@ -75,7 +75,8 @@ int main() {
     scanf("%d", &numEdges);
 
     printf("Enter the edges (node1 node2):\n");
-    for (int i = 0; i < numEdges; i++) {
+    for (int i = 0; i < numEdges; i++) 
+    {
         int node1, node2;
         scanf("%d %d", &node1, &node2);    // Read each edge
         // Populate the adjacency matrix for an undirected graph
