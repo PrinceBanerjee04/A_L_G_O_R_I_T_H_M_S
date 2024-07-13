a27ac893d2391a7c41db4bd39a1b5505dc1761c0
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-07-13 19:33:52 +0530

    Update n_queens.c

--- n_queens.c
+++ n_queens.c
@@ -14,3 +14,24 @@ void printSolution(int board[N][N]) {
         printf("\n");
     }
 }
+
+bool isSafe(int board[N][N], int row, int col) {
+    int i, j;
+
+    // Check this row on the left side
+    for (i = 0; i < col; i++)
+        if (board[row][i] == 1)
+            return false;
+
+    // Check the upper diagonal on the left side
+    for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
+        if (board[i][j] == 1)
+            return false;
+
+    // Check the lower diagonal on the left side
+    for (i = row, j = col; j >= 0 && i < N; i++, j--)
+        if (board[i][j] == 1)
+            return false;
+
+    return true;
+}
