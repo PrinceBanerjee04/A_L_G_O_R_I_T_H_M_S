29d4a555435ff6b7c5277a57e788a4bed774cc02
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-07-14 22:10:38 +0530

    Update n_queens.c

--- n_queens.c
+++ n_queens.c
@@ -5,7 +5,8 @@
 
 void printSolution(int board[N][N]) 
 {
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < N; i++) 
+    {
         for (int j = 0; j < N; j++) {
             if (board[i][j] == 1)
                 printf(" Q ");
