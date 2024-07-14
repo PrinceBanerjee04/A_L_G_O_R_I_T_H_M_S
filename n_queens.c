d9cfd79231638343664ef0a7aea86823f310d00b
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-07-14 22:10:29 +0530

    Update n_queens.c

--- n_queens.c
+++ n_queens.c
@@ -3,7 +3,8 @@
 
 #define N 8 // You can change this value for different sizes of the board
 
-void printSolution(int board[N][N]) {
+void printSolution(int board[N][N]) 
+{
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             if (board[i][j] == 1)
