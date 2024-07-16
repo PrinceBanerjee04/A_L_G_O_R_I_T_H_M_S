e8cf890ed570c265d2c23e4870833b21d0bf7a09
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-07-16 17:13:49 +0530

    Update rat_maze.c

--- rat_maze.c
+++ rat_maze.c
@@ -17,3 +17,34 @@ void printSolution(int sol[N][N]) {
 bool isSafe(int maze[N][N], int x, int y) {
     return (x >= 0 && x < N && y >= 0 && y < N && maze[x][y] == 1);
 }
+
+// A recursive utility function to solve the Maze problem
+bool solveMazeUtil(int maze[N][N], int x, int y, int sol[N][N]) {
+    // If x, y is the destination cell, return true
+    if (x == N - 1 && y == N - 1) {
+        sol[x][y] = 1;
+        return true;
+    }
+
+    // Check if maze[x][y] is a valid move
+    if (isSafe(maze, x, y)) {
+        // Mark x, y as part of the solution path
+        sol[x][y] = 1;
+
+        // Move forward in the x direction
+        if (solveMazeUtil(maze, x + 1, y, sol)) {
+            return true;
+        }
+
+        // Move down in the y direction
+        if (solveMazeUtil(maze, x, y + 1, sol)) {
+            return true;
+        }
+
+        // If neither move is successful, backtrack
+        sol[x][y] = 0;
+        return false;
+    }
+
+    return false;
+}
