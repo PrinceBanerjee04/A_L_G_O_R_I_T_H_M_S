1866b28ef40def1e04d4b41c820e2a0dba4f5be5
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-07-17 21:26:29 +0530

    Update rat_maze.c

--- rat_maze.c
+++ rat_maze.c
@@ -4,7 +4,8 @@
 #define N 4 // Define the size of the maze
 
 // Function to print the solution matrix
-void printSolution(int sol[N][N]) {
+void printSolution(int sol[N][N]) 
+{
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             printf(" %d ", sol[i][j]);
