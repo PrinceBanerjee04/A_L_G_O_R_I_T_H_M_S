d76f4a69585fa4d205a74043f3081db1bf6c3f29
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-07-17 21:26:39 +0530

    Update rat_maze.c

--- rat_maze.c
+++ rat_maze.c
@@ -6,7 +6,8 @@
 // Function to print the solution matrix
 void printSolution(int sol[N][N]) 
 {
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < N; i++) 
+    {
         for (int j = 0; j < N; j++) {
             printf(" %d ", sol[i][j]);
         }
