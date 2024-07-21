dc4dd8a7e8bd11a94ff8a949bca00e758c5b6723
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-07-21 23:14:05 +0530

    Update hamiltonian.c

--- hamiltonian.c
+++ hamiltonian.c
@@ -9,3 +9,14 @@ void printSolution(int path[]) {
         printf("%d ", path[i]);
     printf("%d\n", path[0]);
 }
+
+bool isSafe(int v, int graph[V][V], int path[], int pos) {
+    if (graph[path[pos - 1]][v] == 0)
+        return false;
+
+    for (int i = 0; i < pos; i++)
+        if (path[i] == v)
+            return false;
+
+    return true;
+}
