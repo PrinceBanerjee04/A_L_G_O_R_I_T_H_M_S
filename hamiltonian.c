3d0f627f6c8c14fc2711d32abceee00232fff733
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-07-21 23:13:51 +0530

    Update hamiltonian.c

--- hamiltonian.c
+++ hamiltonian.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+#include <stdbool.h>
 
+#define V 5
+
+void printSolution(int path[]) {
+    printf("Hamiltonian Cycle: ");
+    for (int i = 0; i < V; i++)
+        printf("%d ", path[i]);
+    printf("%d\n", path[0]);
+}
