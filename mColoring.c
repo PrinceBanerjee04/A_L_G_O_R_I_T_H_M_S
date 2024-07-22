711f22a1fb99cb413c7e1a5a7f3bd27d46571973
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-07-22 19:48:15 +0530

    Update mColoring.c

--- mColoring.c
+++ mColoring.c
@@ -14,3 +14,18 @@ bool isSafe(int v, int graph[V][V], int color[], int c) {
             return false;
     return true;
 }
+bool graphColoringUtil(int graph[V][V], int m, int color[], int v) {
+    if (v == V)
+        return true;
+
+    for (int c = 1; c <= m; c++) {
+        if (isSafe(v, graph, color, c)) {
+            color[v] = c;
+            if (graphColoringUtil(graph, m, color, v + 1))
+                return true;
+            color[v] = 0; // backtrack
+        }
+    }
+
+    return false;
+}
