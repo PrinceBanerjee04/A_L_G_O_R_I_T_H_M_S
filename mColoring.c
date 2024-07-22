c92b76ff0c65ed9896f47111a70bd6ce103215ba
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-07-22 19:47:42 +0530

    Update mColoring.c

--- mColoring.c
+++ mColoring.c
@@ -2,3 +2,9 @@
 #include <stdbool.h>
 
 #define V 4
+void printSolution(int color[]) {
+    printf("Solution Exists: Following are the assigned colors\n");
+    for (int i = 0; i < V; i++)
+        printf(" %d ", color[i]);
+    printf("\n");
+}
