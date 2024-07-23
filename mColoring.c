1a1624ab54dacede0d4f7f2b7351b8d7fc13a0e8
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-07-23 22:50:36 +0530

    Update mColoring.c

--- mColoring.c
+++ mColoring.c
@@ -11,7 +11,8 @@ void printSolution(int color[])
     printf("\n");
 }
 
-bool isSafe(int v, int graph[V][V], int color[], int c) {
+bool isSafe(int v, int graph[V][V], int color[], int c) 
+{
     for (int i = 0; i < V; i++)
         if (graph[v][i] && c == color[i])
             return false;
