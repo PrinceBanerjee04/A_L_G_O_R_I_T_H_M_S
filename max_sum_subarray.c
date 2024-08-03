7b27c284433f9d41bb0fe3892e9b1ab6703b1921
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-08-03 21:59:02 +0530

    Update max_sum_subarray.c

--- max_sum_subarray.c
+++ max_sum_subarray.c
@@ -4,3 +4,13 @@ int max(int a, int b)
 {
     return (a > b) ? a : b;
 }
+int MaxSumDP(int arr[],int n)
+{
+    int current_sum = arr[0], max_sum = arr[0];
+    for(int i=1;i<n;i++)
+    {
+        current_sum = max(arr[i],current_sum+arr[i]);
+        max_sum = max(current_sum,max_sum);
+    }
+    return max_sum;
+}
